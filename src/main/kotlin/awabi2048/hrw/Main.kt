13f826e006224ff13229ad8a66ccb3c7614c8967
awabi2048 <2886xl@gmail.com>
2025-03-22 19:08:46 +0900

    init

---
+++ src/main/kotlin/awabi2048/hrw/Main.kt
@@ -0,0 +1,14 @@
+package awabi2048.hrw
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
